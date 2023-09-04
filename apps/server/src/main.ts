01d45203c6f70a969f86d55cb190dafe367496f8
ayoung4 <alfredsyoung@gmail.com>
2023-09-04 15:18:00 -0700

    add socket io

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,1 +1,19 @@
-console.log('Hello World');
+import express from 'express'
+import * as http from 'http'
+import { Server } from 'socket.io'
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+})
+
+server.listen(3000, () => {
+  console.log('listening on *:3000')
+})
