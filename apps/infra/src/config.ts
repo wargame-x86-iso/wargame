60222b028c5278235b731f112ddc3aded42b6298
ayoung4 <alfredsyoung@gmail.com>
2023-09-11 21:43:23 -0700

    debug config change

--- apps/infra/src/config.ts
+++ apps/infra/src/config.ts
@@ -10,5 +10,5 @@ export const stackSuffix = stack.toUpperCase()
 const github = new pulumi.Config('github')
 export const githubRepository = github.require('repository')
 
-const wargame = new pulumi.Config('gcp_wargame')
+const wargame = new pulumi.Config('wargame')
 export const altLocation = wargame.require('alt_region')
